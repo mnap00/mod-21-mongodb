53356a79c35253f1f983cf3cd4a826a6a82cb51c
mnap <learn@10g.pl>
2018-05-26 14:49:57 +0200

    Add userSchema

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://localhost/nodeappdatabase', {
     useMongoClient: true
+});
+
+const userSchema = new Schema({
+    name: String,
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    admin: Boolean
 });
