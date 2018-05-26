a0bac97bba08d34e51d47e1e89ca0f34d0091f6c
mnap <learn@10g.pl>
2018-05-26 19:58:39 +0200

    Add find method with promise

--- server.js
+++ server.js
@@ -77,3 +77,11 @@ mark.save(function(err) {
     console.log('User ' + mark.name + ' saved successfuly');
 });
 
+const query = User.find({});
+const promise = query.exec();
+promise.then(function(records) {
+    console.log('Actual database records are ' + records);
+});
+promise.catch(function(reason) {
+    console.log('Something went wrong: ' + reason);
+});
