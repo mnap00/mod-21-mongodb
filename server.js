9e161c3cb8e558164ecc1b25e6ccc8c2645ccb06
mnap <learn@10g.pl>
2018-05-26 17:39:54 +0200

    Add userSchema methods

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/nodeappdatabase', {
     useMongoClient: true
 });
@@ -9,8 +10,24 @@ const userSchema = new Schema({
     name: String,
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    admin: Boolean
+    admin: Boolean,
+    created_at: Date,
+    updated_at: Date
 });
 
+userSchema.methods.manify = function(next) {
+    this.name = this.name + '-boy';
+    return next(null, this.name);
+};
+
+userSchema.pre('save', function(next) {
+    const currentDate = new Date();
+    this.updated_at = currentDate;
+    if (!this.created_at) {
+        this.created_at = currentDate;
+    }
+    next();
+});
 
 const User = mongoose.model('User', userSchema);
+
